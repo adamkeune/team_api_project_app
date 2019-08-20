845b4bd93b72746c3933a96f53e96c9bdb81ea31
Adam Keune <akeune23@gmail.com>
2019-08-20 18:33:16 -0500

    Add students update and destroy routes

--- config/routes.rb
+++ config/routes.rb
@@ -21,5 +21,7 @@ Rails.application.routes.draw do
     get "/students" => "students#index"
     get "/students/:id" => "students#show"
     post "/students" => "students#create"
+    patch "/students/:id" => "students#update"
+    delete "/students/:id" => "students#destroy"
   end
 end
