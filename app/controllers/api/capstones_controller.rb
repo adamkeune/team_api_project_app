6538457e1fb0df227a87f720b04432f0ca89d041
Fiona Ian <sayhitofiona@gmail.com>
2019-08-19 20:31:46 -0500

    Added Skills and Capstones Controllers

---
+++ app/controllers/api/capstones_controller.rb
@@ -0,0 +1,41 @@
+class Api::CapstonesController < ApplicationController
+def index
+    @capstones = Capstone.all
+    render "index.json.jb"
+  end
+
+  def create
+    @capstone = Capstone.new(
+      name: params[:name],
+      description: params[:description],
+      url: params[:url],
+      screenshot_url: params[:screenshot_url]
+      )
+    if @capstone.save
+      render "show.json.jb"
+    else
+      render json: { errors: @capstone.errors.full_messages }, status: 422
+    end
+  end
+
+  def show
+    @capstone = Capstone.find_by(id: params[:id])
+    render "show.json.jb"
+  end
+
+  def update
+    @capstone = Capstone.find_by(id: params["id"])
+    @capstone.name = params["name"] || @capstone.name,
+    @capstone.description = params["description"] || @capstone.description,
+    @capstone.url = params["url"] || @capstone.url,
+    @capstone.screenshot_url = params["screenshot_url"] || @capstone.screenshot_url,
+    @capstone.save
+    render "show.json.jb"
+  end
+
+  def destroy
+    @capstone = Capstone.find_by(id: params["id"])
+    @capstone.destroy
+    render json: { message: "Capstone successfully destroyed!" }
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
 
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/skills" => "skills#index"
+    post "/skills" => "skills#create"
+    get "/skills/:id" => "skills#show"
+    patch "/skills/:id" => "skills#update"
+    delete "/skills/:id" => "skills#destroy"
+
+    get "/capstones" => "capstones#index"
+    post "/capstones" => "capstones#create"
+    get "/capstones/:id" => "capstones#show"
+    patch "/capstones/:id" => "capstones#update"
+    delete "/capstones/:id" => "capstones#destroy"
+  end
 end
